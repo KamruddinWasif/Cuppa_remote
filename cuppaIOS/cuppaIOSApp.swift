df4ac967e34f52684c5c23ebeca3db0ce300ed94
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-04-16 23:06:31 +1000

    Initial Commit

---
+++ cuppaIOS/cuppaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cuppaIOSApp.swift
+//  cuppaIOS
+//
+//  Created by Wasif Kamruddin on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct cuppaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
